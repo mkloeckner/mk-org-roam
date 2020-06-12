c62b4b2d043e55581e0ca451ac3322501f70d2d4
M, Kloeckner <493973+mkloeckner@users.noreply.github.com>
2020-06-12 10:43:53 +0200

    initial commit

---
+++ packages.el
@@ -0,0 +1,27 @@
+(defconst mk-org-roam-packages
+  '(org-roam))
+
+(defun mk-org-roam/init-org-roam ()
+  (use-package org-roam
+    :hook
+    (after-init . org-roam-mode)
+    :custom
+    (org-roam-directory "/path/to/org-files/")
+    :init
+    (progn
+      (spacemacs/declare-prefix "ar" "org-roam")
+      (spacemacs/set-leader-keys
+        "arl" 'org-roam
+        "art" 'org-roam-dailies-today
+        "arf" 'org-roam-find-file
+        "arg" 'org-roam-graph)
+
+      (spacemacs/declare-prefix-for-mode 'org-mode "mr" "org-roam")
+      (spacemacs/set-leader-keys-for-major-mode 'org-mode
+        "rl" 'org-roam
+        "rt" 'org-roam-dailies-today
+        "rb" 'org-roam-switch-to-buffer
+        "rf" 'org-roam-find-file
+        "ri" 'org-roam-insert
+        "rg" 'org-roam-graph))))
+
